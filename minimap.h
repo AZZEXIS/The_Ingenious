112b2a5ee9b99b5f7ac2c96bfead0eab5c181d31
Mohamed_Aziz_Belkahla <mohamedaziz.belkahla@esprit.tn>
2022-04-22 14:37:04 +0100

    new repository created

---
+++ minimap.h
@@ -0,0 +1,57 @@
+#ifndef FONCTIONS_H_
+#define FONCTIONS_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL.h>
+#include <string.h>
+#include <SDL/SDL_ttf.h>
+#include <time.h>
+#define SCREEN_W 1914
+#define SCREEN_H 884
+typedef struct 
+{
+	SDL_Surface* img_miniature;
+	SDL_Rect  pos_minimap;
+	SDL_Surface* img_bonhomme;
+	SDL_Rect  pos_bonhomme;
+	
+}minimap;
+typedef struct
+{
+	SDL_Surface* img_perso;
+	SDL_Rect  pos_perso;
+
+}joueur;
+struct Image
+{
+SDL_Rect pos1;
+SDL_Rect pos2;
+SDL_Surface *img;
+};
+typedef struct Image Image;
+typedef struct
+{
+	SDL_Rect position;
+	TTF_Font *font;
+	SDL_Surface *surface_texte;
+	SDL_Color text_color;
+	char text[50];
+
+
+}
+text;
+
+void initminimap(minimap *m);
+void init_Joueur(joueur *j);
+void MAJMinimap(SDL_Rect posJoueur,  minimap * m, SDL_Rect camera, int redimensionnement);
+void afficher (minimap m, SDL_Surface * screen);
+void Liberer (minimap * m);
+void sauvegarder( int score, char nomjoueur[], char nomfichier[]);
+void meilleur( char nomfichier[], int *score, char nomjoueur[]);
+void init_text(text *t);
+void init_text_saisir(text *t);
+void saisir_nom(text t,SDL_Surface *screen);
+void entrer_nom(text t,SDL_Surface *screen);
+#endif
+
